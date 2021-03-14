f9c987dc27a75768ea8b6095a652e17e545f2015
Jason <jasondubourg@gmail.com>
2021-03-14 14:48:27 +0100

    Modification du logger

--- src/main/java/com/safetynet/SafetynetApplication.java
+++ src/main/java/com/safetynet/SafetynetApplication.java
@@ -2,8 +2,12 @@ package com.safetynet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@Configuration
+@EnableAspectJAutoProxy
 public class SafetynetApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/safetynet/api/config/RestLogAspect.java
+++ src/main/java/com/safetynet/api/config/RestLogAspect.java
@@ -1,73 +1,59 @@
 package com.safetynet.api.config;
 
-import java.lang.reflect.Method;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 @Aspect
 public class RestLogAspect {
 
-	Logger log = LoggerFactory.getLogger(this.getClass());
-
-	@Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
-	public void restAnnotation() {
+	@Before("allControllerPointCut()")
+	public void logBeforeAllMethod(JoinPoint joinPoint) {
+		System.out.println("--- BEFORE --- Method " + joinPoint.getSignature().getName() + "() with "
+				+ Arrays.toString(joinPoint.getArgs()));
 	}
 
-	@Around("restAnnotation() && execution(* *(..))")
-	public Object restLog(ProceedingJoinPoint joinPoint) throws Throwable {
-		final MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-		final Method method = signature.getMethod();
+	@After("allControllerPointCut()")
+	public void logAfterAllMethod(JoinPoint joinPoint) {
+		System.out.println("--- AFTER --- Method " + joinPoint.getSignature().getName() + "()"
+				+ Arrays.toString(joinPoint.getArgs()));
+	}
 
-		final Class<?> controllerClass = joinPoint.getTarget().getClass();
-		final RequestMapping controllerAnnotation = controllerClass.getAnnotation(RequestMapping.class);
-		final String controllerRestPath = (controllerAnnotation.path().length == 0) ? ""
-				: controllerAnnotation.path()[0];
+	@AfterThrowing(pointcut = "allControllerPointCut()", throwing = "e")
+	public void logThrowAllMethod(JoinPoint joinPoint, Exception e) throws Throwable {
+		System.out.println("--- EXCEPTION --- Method " + joinPoint.getSignature().getName());
+		System.out.println("--- EXCEPTION DETAILS --- Method " + e.getMessage());
+	}
 
-		final RequestMapping controllerMethodAnnotation = method.getAnnotation(RequestMapping.class);
-		final String methodRestPath = (controllerMethodAnnotation.path().length == 0) ? ""
-				: controllerMethodAnnotation.path()[0];
-		final RequestMethod requestMethod = (controllerMethodAnnotation.method().length == 0) ? RequestMethod.GET
-				: controllerMethodAnnotation.method()[0];
-
-		final Instant start = Instant.now();
-
-		// @Before code
-		Object returnValue = null;
-		Throwable throwable = null;
+	public Object logAroundMethod(ProceedingJoinPoint pjp) throws Throwable {
+		// same with MethodBeforeAdvice
+		System.out.println(" ----Before method avec Around " + pjp.getSignature().getName() + "()");
 		try {
-			returnValue = joinPoint.proceed();
-		} catch (Throwable throwable1) {
-			throwable = throwable1;
-			log.warn(String.format("[RestLogAspect] : %s", throwable1.getMessage()));
+			// proceed to original method call
+			Object result = pjp.proceed();
+			// same with AfterReturningAdvice
+			System.out.println(" ----After method avec Around " + pjp.getSignature().getName() + "()"
+					+ " avec le resultat " + result);
+			return result;
+
+		} catch (Throwable e) {
+			// same with ThrowsAdvice
+			System.out.println("----AfterThrowing method avec Around  " + pjp.getSignature().getName() + "()"
+					+ ":et leve exception!");
+			throw e;
 		}
-
-		// @After code
-		final String logMessage = String.format("[REST][%sms][%s][%s][%s][%s()] %s",
-				Duration.between(start, Instant.now()).toMillis(), controllerClass.getSimpleName(), requestMethod,
-				controllerRestPath + methodRestPath, method.getName(),
-				Arrays.stream(joinPoint.getArgs()).map(argument -> (argument != null) ? argument.toString() : "null")
-						.collect(Collectors.joining(", ")));
-
-		log.info(logMessage);
-
-		if (throwable != null) {
-			throw throwable;
-		}
-
-		return returnValue;
 	}
+
+	@Pointcut("execution(* com.safetynet.api.controller.*.*(..))")
+	public void allControllerPointCut() {
+	}
+
 }
